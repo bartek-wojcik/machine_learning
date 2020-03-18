e810712bb9a9d5934e9482e102114da428a5260e
unknown <bartekwwb@gmail.com>
2020-03-18 12:02:43 +0100

    Adderd graphs

--- classification.py
+++ classification.py
@@ -2,10 +2,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree, export_graphviz
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from abc import ABC, abstractmethod
+import graphviz
 
 
 class Dataset(ABC):
@@ -49,13 +50,31 @@ class CreditCardDataset(Dataset):
         self.X = self.df.drop(['Time', 'Class'], axis=1)
 
 
-class Classifier:
+class Classifier(ABC):
     score = None
+    result = None
+    name = None
+    model = None
 
-    def __init__(self, data, model):
+    def __init__(self, name, data):
         x_train, x_test, y_train, y_test = data
-        model.fit(x_train, y_train)
-        self.score = model.score(x_test, y_test)
+        self.result = self.model.fit(x_train, y_train)
+        self.score = self.model.score(x_test, y_test)
+        self.plot()
+
+    @abstractmethod
+    def plot(self):
+        pass
+
+
+class DecisionTree(Classifier):
+
+    model = DecisionTreeClassifier()
+
+    def plot(self):
+        data = export_graphviz(self.result, out_file=None)
+        graph = graphviz.Source(data)
+        graph.render(self.name)
 
 
 datasets = {
@@ -65,16 +84,17 @@ datasets = {
 }
 
 classifiers = {
-    'decision tree': DecisionTreeClassifier(),
+    'decision tree': DecisionTree,
     'bayes': GaussianNB(),
-    'vectors': SVC(),
+    'vectors': SVC(C=1),
     'knn': KNeighborsClassifier(),
-    'random forest': RandomForestClassifier(),
+    'random forest': RandomForestClassifier(n_estimators=100),
 }
 
 for dataset_name, dataset in datasets.items():
     for classifier_name, classifier in classifiers.items():
-        result = Classifier(dataset.data, classifier)
+        result = classifier(dataset_name, dataset.data)
         print('Dataset: {} Classifier: {} Score: {}'.format(
             dataset_name, classifier_name, result.score
         ))
+
