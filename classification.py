e7132e9a220e45d0d8ba90c0ef5f23b830f06318
Anewil <bartekwwb@gmail.com>
2020-03-24 12:51:41 +0100

    Added params support

--- classification.py
+++ classification.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier, plot_tree, export_graphviz
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -57,6 +57,7 @@ class Classifier(ABC):
     model = None
 
     def __init__(self, name, data):
+        self.name = name
         x_train, x_test, y_train, y_test = data
         self.result = self.model.fit(x_train, y_train)
         self.score = self.model.score(x_test, y_test)
@@ -68,33 +69,55 @@ class Classifier(ABC):
 
 
 class DecisionTree(Classifier):
-
     model = DecisionTreeClassifier()
 
     def plot(self):
+        print(self.name + ' Max depth: ' + str(self.result.tree_.max_depth))
         data = export_graphviz(self.result, out_file=None)
         graph = graphviz.Source(data)
         graph.render(self.name)
 
 
+class RandomForest(Classifier):
+    model = RandomForestClassifier()
+
+    def plot(self):
+        data = export_graphviz(self.result.estimators_[5], out_file=None)
+        graph = graphviz.Source(data)
+        graph.render(self.name)
+
+
+class Vectors(Classifier):
+    model = LinearSVC(max_iter=1000, C=1000)
+
+    def plot(self):
+        pass
+    
+    
+class Knn(Classifier):
+    model = KNeighborsClassifier(n_neighbors=5)
+
+    def plot(self):
+        pass
+
+
 datasets = {
-    'shopper': ShoppersDataset('online_shoppers_intention.csv'),
     'heart': HeartDataset('heart.csv'),
+    'shopper': ShoppersDataset('online_shoppers_intention.csv'),
     'credit card': CreditCardDataset('creditcard.csv'),
 }
 
 classifiers = {
     'decision tree': DecisionTree,
-    'bayes': GaussianNB(),
-    'vectors': SVC(C=1),
-    'knn': KNeighborsClassifier(),
-    'random forest': RandomForestClassifier(n_estimators=100),
+    'bayes': GaussianNB,
+    'vectors': Vectors,
+    'knn': Knn,
+    'random forest': RandomForest,
 }
 
 for dataset_name, dataset in datasets.items():
     for classifier_name, classifier in classifiers.items():
-        result = classifier(dataset_name, dataset.data)
+        result = classifier(dataset_name + ' ' + classifier_name, dataset.data)
         print('Dataset: {} Classifier: {} Score: {}'.format(
             dataset_name, classifier_name, result.score
         ))
-
