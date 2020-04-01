4b94c251bd85998c7162cd58e134148d86fc6a81
unknown <bartekwwb@gmail.com>
2020-04-01 20:04:31 +0200

    Added tests

--- clustering.py
+++ clustering.py
@@ -4,6 +4,7 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler, normalize
 from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Dataset(ABC):
@@ -14,10 +15,10 @@ class Dataset(ABC):
         self.scale()
         self.normalize()
         self.pca()
-        
+
     def prepare_data(self):
         pass
-    
+
     def scale(self):
         scaler = StandardScaler()
         self.data = scaler.fit_transform(self.data)
@@ -39,23 +40,62 @@ class ShoppersDataset(Dataset):
         self.data.fillna(0, inplace=True)
 
 
+class HeartDataset(Dataset):
+    pass
+
+
+class CreditCardDataset(Dataset):
+    pass
+
+
 class ClusterDBSCAN:
     clustering = None
 
-    def __init__(self, dataset):
-        self.data = dataset.data
-        self.cluster()
+    def __init__(self, dataset, eps, min_samples, name):
+        self.name = name
+        self.data = dataset.data.copy()
+        self.cluster(eps, min_samples)
 
-    def cluster(self):
-        self.clustering = DBSCAN(eps=0.07, min_samples=50).fit(self.data)
-        labels = self.clustering.labels_
-        plt.scatter(self.data['P1'], self.data['P2'], c=labels)
+    def cluster(self, eps, min_samples):
+        self.clustering = DBSCAN(eps=eps, min_samples=min_samples).fit(self.data)
+        groups = self.clustering.labels_
+        self.data['COLORS'] = groups
+        for group in np.unique(groups):
+            label = 'Cluster {}'. format(group) if group != -1 else 'Noise points'
+            filtered_group = self.data[self.data['COLORS'] == group]
+            plt.scatter(filtered_group['P1'], filtered_group['P2'], label=label)
+        n_clusters_ = len(set(groups)) - (1 if -1 in groups else 0)
+        n_noise_ = list(groups).count(-1)
+        print('Estimated number of clusters: %d' % n_clusters_)
+        print('Estimated number of noise points: %d' % n_noise_)
+        plt.title(self.name + ' dataset')
+        plt.figtext(0.05, 0.8, 'Eps: {}\nMin_samples: {}\nNumber of clusters: {}\nNoise points: {}'.format(
+            eps, min_samples, n_clusters_, n_noise_
+        ), fontsize=15)
+        fig = plt.gcf()
+        fig.set_size_inches(14, 8)
+        plt.legend()
         plt.show()
 
 
 if __name__ == "__main__":
     datasets = {
         'shopper': ShoppersDataset('online_shoppers_intention.csv'),
+        'heart': HeartDataset('heart.csv'),
+        'creditcard': CreditCardDataset('creditcard.csv'),
     }
+    ClusterDBSCAN(datasets['heart'], 0.1, 20, name='Heart')
+    ClusterDBSCAN(datasets['heart'], 0.15, 20, name='Heart')
+    ClusterDBSCAN(datasets['heart'], 0.2, 20, name='Heart')
 
-    ClusterDBSCAN(datasets['shopper'])
+    ClusterDBSCAN(datasets['heart'], 0.15, 5, name='Heart')
+    ClusterDBSCAN(datasets['heart'], 0.15, 10, name='Heart')
+    ClusterDBSCAN(datasets['heart'], 0.15, 20, name='Heart')
+
+    ClusterDBSCAN(datasets['shopper'], 0.05, 10, name='Shopper')
+    ClusterDBSCAN(datasets['shopper'], 0.05, 20, name='Shopper')
+    ClusterDBSCAN(datasets['shopper'], 0.05, 30, name='Shopper')
+    ClusterDBSCAN(datasets['shopper'], 0.05, 60, name='Shopper')
+    ClusterDBSCAN(datasets['shopper'], 0.05, 100, name='Shopper')
+
+    # ClusterDBSCAN(datasets['creditcard'], 0.1, 100)
